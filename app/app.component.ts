ec620dcae5a07eb00be74cdde85ad66d517b91ef
Luis Alberto Gómez Rodríguez <alberto.gomez@cargomovil.com>
2016-01-20 13:14:34 -0600

    (chore)Editing Our Hero

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,10 @@ import {Component} from 'angular2/core';
     template: `<h1>{{title}}</h1>
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
-    <div><label>name: </label>{{hero.name}}</div>
+    <div>
+        <label>name: </label>
+        <div><input [(ngModel)] = "hero.name" placeholder="name"></div>
+    </div>
     `
 })
 export class AppComponent { 
